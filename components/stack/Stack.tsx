ca2e2aef86bdcdab51738b0730a636c9e52158e5
azulcolor <lealpastelin@gmail.com>
2024-12-30 14:11:07 -0500

    improving responsive

--- components/stack/Stack.tsx
+++ components/stack/Stack.tsx
@@ -9,7 +9,7 @@ export const Stack = () => {
           <p className="text-gray-200">That I have</p>
         </div>
 
-        <p className="text-gray-100 text-3xl w-2/3 text-center">
+        <p className="text-gray-100 text-2xl px-6 md:text-3xl md:w-2/3 text-center">
           I transform concepts into dynamic web experiences. Code is my craft, user satisfaction is my goal
         </p>
       </div>
