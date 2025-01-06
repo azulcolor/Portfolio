6042de82ce675da4a3cc9ee4636e396d1aed92a4
azulcolor <lealpastelin@gmail.com>
2025-01-06 17:26:27 -0500

    changing framer component for a css animation

--- components/stack/components/images/Images.tsx
+++ components/stack/components/images/Images.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
-import { ParallaxImages } from "./components";
 import { imagesPath } from "./constants";
 
 export const Images = () => {
   return (
-    <div>
-      <ParallaxImages baseVelocity={3}>
+    <div className="slider">
+      <div className="slide-track">
         {imagesPath.map((image, index) => (
-          <Image key={index} src={image} width={40} height={40} alt={index.toString()} />
+          <div className="slide" key={index}>
+            <Image key={index} src={image} width={40} height={40} alt={index.toString()} />
+          </div>
         ))}
-      </ParallaxImages>
+        {imagesPath.map((image, index) => (
+          <div className="slide" key={index}>
+            <Image key={index} src={image} width={40} height={40} alt={index.toString()} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/ui/header/hooks/useChangeColor.tsx
+++ components/ui/header/hooks/useChangeColor.tsx
@@ -7,20 +7,17 @@ export const useChangeColor = () => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          console.log(entry)
           if (entry.isIntersecting) {
-              // Cambia el color del header según la clase de la sección
               setIsDarkSection(entry.target.classList.contains("dark-section"));
           }
         });
       },
       {
-        root: null, // Observa en el viewport completo
-        threshold: .9, // Asegúrate de que la sección ocupa al menos el 90% del viewport
+        root: null,
+        threshold: .9,
       }
     );
 
-    // Observa todas las secciones
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => observer.observe(section));
 
